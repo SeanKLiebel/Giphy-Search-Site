542b22a77cab46f19a6e016834537142e1a59f96
SeanKLiebel <61285232+SeanKLiebel@users.noreply.github.com>
2020-04-29 23:05:16 -0400

    Update main.js
    First working code example of my site with a successful api call

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,31 @@
+var $form = $("form"),
+    $search = $(".search"),
+    $clear = $(".clear"),
+    $giphy = $(".giphy img"),
+    $giphyLink = $(".giphy a");
 
+
+$form.on("submit", function(e) {
+    e.preventDefault();
+    goGiphy();
+});
+
+$clear.on("click", function() {
+    $search.val("");
+})
+
+function goGiphy() {
+    var input = $search.val();
+    $.getJSON("https://api.giphy.com/v1/gifs/random?api_key=cRsviw3H08yzisru1J1oc0VVAGt1cZ0f&tag="+ input, function (json) {
+                       data = JSON.parse(JSON.stringify(json));
+                       imgSrc = data.data.images.original.url;
+                       $giphy.fadeOut(1000);
+                       setTimeout(function() {
+                           $giphy.attr("src", imgSrc);
+                           $giphyLink.attr("href", imgSrc);
+                           setTimeout(function () {
+                              $giphy.addClass("gif");
+                              $giphy.fadeIn(1000);
+                           },800);
+                       }, 800);
+    })
