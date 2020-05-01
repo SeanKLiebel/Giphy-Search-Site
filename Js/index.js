b07b8484c45190812589481d6343cf5dde33487b
SeanKLiebel <61285232+SeanKLiebel@users.noreply.github.com>
2020-05-01 18:26:16 -0400

    Update index.js
    Created fetch methods with Javascript. Functions for receiving and posting gifs were made and successful in producing outputs.

--- Js/index.js
+++ Js/index.js
@@ -1,31 +1,50 @@
-var $form = $("form"),
-    $search = $(".search"),
-    $clear = $(".clear"),
-    $giphy = $(".giphy img"),
-    $giphyLink = $(".giphy a");
-
-
-$form.on("submit", function(e) {
-    e.preventDefault();
-    goGiphy();
-});
-
-$clear.on("click", function() {
-    $search.val("");
-})
-
-function goGiphy() {
-    var input = $search.val();
-    $.getJSON("https://api.giphy.com/v1/gifs/random?api_key=cRsviw3H08yzisru1J1oc0VVAGt1cZ0f&tag="+ input, function (json) {
-                       data = JSON.parse(JSON.stringify(json));
-                       imgSrc = data.data.images.original.url;
-                       $giphy.fadeOut(1000);
-                       setTimeout(function() {
-                           $giphy.attr("src", imgSrc);
-                           $giphyLink.attr("href", imgSrc);
-                           setTimeout(function () {
-                              $giphy.addClass("gif");
-                              $giphy.fadeIn(1000);
-                           },800);
-                       }, 800);
-    })
+function getGiphyData() {
+    //Get giphy data
+    var searchTerm = document.querySelector("#search").value;
+    var url =
+      "https://api.giphy.com/v1/gifs/search?api_key=wOkuLOpg0lyr0TN1NMtr0qIAsJpFW7rn&q=" +
+      searchTerm;
+  
+    fetch(url)
+      .then(data => data.json())
+      .then(res => {
+        console.log(res);
+  
+        var arrayOfGifs = res.data;
+        var rand = Math.floor(Math.random() * arrayOfGifs.length);
+        var firstItem = arrayOfGifs[rand];
+        var giphyLink = firstItem.images.fixed_width.url;
+        //makes giphy appear on page
+        document.querySelector("#gif").setAttribute("src", giphyLink);
+      })
+      .catch(error => console.log(error));
+  }
+  
+  function getTrendingData() {
+    var url =
+      "https://api.giphy.com/v1/gifs/trending?api_key=wOkuLOpg0lyr0TN1NMtr0qIAsJpFW7rn&q";
+  
+    fetch(url)
+      .then(data => data.json())
+      .then(res => {
+        console.log(res);
+        var _outputHTML = ""
+        var _tenGifs = document.getElementById("tenGifs");
+      //  document.querySelector("#gif1").setAttribute("src", giphyLink);
+        if (res.data.length < 10) return; // if not ten quit for now
+        for(let i=0; i< 10 ;i++) {
+          // add 10 gifs by appending new elements to the div #tenGifs
+          var node = document.getElementById("gif~"+i);      
+          node.setAttribute("src", res.data[i].images.fixed_width.url);
+          _tenGifs.appendChild(node);  
+          
+        }
+        
+        
+      });
+  }
+  
+  function setFavorite(imgId) {
+    // save favorite for later
+  }
+  
